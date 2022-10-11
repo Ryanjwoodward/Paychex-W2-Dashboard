21ffd0a989aed0484961a1d7a8866600a58d55c1
Ryan Woodward <ryanjwoodward@outlook.com>
2022-10-11 14:25:39 -0700

    Created Directories Added Springboot Project
    Added a new directory to store files related to MySQL and Springboot. Created Springboot project and added it to the backend folder

---
+++ Back-End/Springboot/Paychex-Dashboard/src/main/java/com/gcucapstone/paychexdashboard/PaychexDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.gcucapstone.paychexdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaychexDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaychexDashboardApplication.class, args);
+	}
+
+}
---
+++ Back-End/Springboot/Paychex-Dashboard/src/test/java/com/gcucapstone/paychexdashboard/PaychexDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gcucapstone.paychexdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaychexDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
